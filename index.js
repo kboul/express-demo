974177f35c62acda8b00cc2c2b7b7a35762dab47
kostas <konstantinos.voulgaridis@aspisid.com>
2019-09-26 12:07:43 +0300

    Replace dummy array with an array of courses, implement & handle courses/:id route

--- index.js
+++ index.js
@@ -2,12 +2,32 @@ const express = require('express');
 
 const app = express();
 
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
+
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const {
+        params: { id }
+    } = req;
+
+    const selectedCourse = courses.find(course => course.id === parseInt(id));
+
+    if (!selectedCourse) {
+        res.status(404).send(`The course with the id ${id} was not found`);
+    }
+
+    res.send(selectedCourse);
 });
 
 const port = process.env.PORT || 5000;
