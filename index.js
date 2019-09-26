e079d5ababb2ff013af5b9374bb42ae4a23f80fb
kostas <konstantinos.voulgaridis@aspisid.com>
2019-09-26 11:39:08 +0300

    Configure port to listen as an environmental variable

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    console.log(req);
-    console.log(res);
-    res.send('Hello World');
+    res.send('Hello World!!!');
 });
 
 app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
