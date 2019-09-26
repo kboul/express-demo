e117150e16840e97154cfa74b40b3dc38a836832
kboul <kos.voulgaridis@gmail.com>
2019-09-26 12:29:00 +0300

    Handle HTTP post request

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+// enable parsing of JSON objects
+// in the body of the request by
+// adding a piece of middleware...
+app.use(express.json());
+
 const courses = [
     { id: 1, name: 'course1' },
     { id: 2, name: 'course2' },
@@ -14,6 +19,16 @@ app.get('/', (req, res) => {
 
 app.get('/api/courses', (req, res) => {
     res.send(courses);
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
 });
 
 app.get('/api/courses/:id', (req, res) => {
