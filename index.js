7042af1544b229cd3650134ee4e99e141a6dc546
kboul <kos.voulgaridis@gmail.com>
2019-09-30 11:07:30 +0300

    Connect to MongoDB

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const config = require('config');
 const startupDebugger = require('debug')('app:startup');
+const mongoose = require('mongoose');
+
 const logger = require('./middlewares/logger.js');
 const home = require('./routes/home');
 const courses = require('./routes/courses');
@@ -32,6 +34,16 @@ console.log(`Application name: ${config.get('name')}`);
 // console.log(`Application name: ${config.mail.host}`);
 // should declare env variable app_DB_passwordbefore
 // console.log(`DB password: ${config.DB.password}`);
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+mongoose
+    .connect('mongodb://localhost/playground', options)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
 
 // use template engine
 app.set('view engine', 'pug');
