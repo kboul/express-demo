430e79e45da2f3fb9fe14da941669999a097ecd9
kboul <kos.voulgaridis@gmail.com>
2019-09-30 11:18:46 +0300

    Introduction to schema, create a courseShema variable

--- index.js
+++ index.js
@@ -45,6 +45,14 @@ mongoose
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
 // use template engine
 app.set('view engine', 'pug');
 app.set('views', './views'); // default views folder should be in the route of the app
