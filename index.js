cbd026e6c59d074fca72c8aedb62dc6305a6f9c2
kboul <kos.voulgaridis@gmail.com>
2019-09-30 15:59:58 +0300

    Updating a document in MongoDB using both query first and update first approaches

--- index.js
+++ index.js
@@ -75,6 +75,39 @@ const getCourses = async () => {
 
 getCourses();
 
+const updateCourseQueryFirst = async id => {
+    const course = await Course.findById(id);
+    if (!course) return;
+
+    course.isPublished = false;
+    course.author = 'Another Author';
+
+    /*
+        or course.set({
+            isPublised: true,
+            author: 'Another Author'
+        })
+    */
+
+    const result = await course.save();
+    console.log(result);
+};
+
+const updateCourseUpdateFirst = async id => {
+    const result = await Course.update(
+        { _id: id },
+        {
+            $set: {
+                author: 'Mosh',
+                isPublished: true
+            }
+        }
+    );
+    console.log(result);
+};
+// updateCourseQueryFirst('5d91beb5fb0b175aa1858174');
+updateCourseUpdateFirst('5d91beb5fb0b175aa1858174');
+
 // use template engine
 app.set('view engine', 'pug');
 app.set('views', './views'); // default views folder should be in the route of the app
